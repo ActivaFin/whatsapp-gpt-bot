9417a0f34e558661fdab1efe8f7ae47014b9ad4a
Activa Fin Limitada <136639885+ActivaFin@users.noreply.github.com>
2025-02-12 17:56:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -58,4 +58,5 @@ def webhook():
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
